c1ff44f2585fe7572eed3c454d1df020ef9ca3e7
Nguyen Trang <dii.afyra.work@gmail.com>
2025-07-28 21:33:18 +0700

    theme color

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Default theme còn các theme khác thì phát triển sau
+  static const Color defaultTheme = Color(0xFFFFFFFF);
+
+  // Ocean Blue palette
+  static const Color oceanBlue50 = Color(0xFFF1F7F9);
+  static const Color oceanBlue100 = Color(0xFFDFEDF1);
+  static const Color oceanBlue200 = Color(0xFFC3DDE5);
+  static const Color oceanBlue300 = Color(0xFFA3CBD7);
+  static const Color oceanBlue400 = Color(0xFF86BBCA);
+  static const Color oceanBlue500 = Color(0xFF66A9BC);
+  static const Color oceanBlue600 = Color(0xFF4B96AC);
+  static const Color oceanBlue700 = Color(0xFF386F80);
+  static const Color oceanBlue800 = Color(0xFF254A55);
+  static const Color oceanBlue900 = Color(0xFF13252B);
+  static const Color oceanBlue950 = Color(0xFF091315);
+
+  // Indigo Night palette
+  static const Color indigoNight50 = Color(0xFFEEF1F7);
+  static const Color indigoNight100 = Color(0xFFE0E5F0);
+  static const Color indigoNight200 = Color(0xFFBDC9E0);
+  static const Color indigoNight300 = Color(0xFF9DAFD2);
+  static const Color indigoNight400 = Color(0xFF7A93C2);
+  static const Color indigoNight500 = Color(0xFF5B79B3);
+  static const Color indigoNight600 = Color(0xFF476399);
+  static const Color indigoNight700 = Color(0xFF374D77);
+  static const Color indigoNight800 = Color(0xFF253450);
+  static const Color indigoNight900 = Color(0xFF121926);
+  static const Color indigoNight950 = Color(0xFF0A0E15);
+
+  // Primary palette
+  static const Color primary50 = Color(0xFFF8F3EC);
+  static const Color primary100 = Color(0xFFF3EADD);
+  static const Color primary200 = Color(0xFFE5D3B8);
+  static const Color primary300 = Color(0xFFD9BE96);
+  static const Color primary400 = Color(0xFFCCA873);
+  static const Color primary500 = Color(0xFFBE8F4C);
+  static const Color primary600 = Color(0xFF9D7539);
+  static const Color primary700 = Color(0xFF74562A);
+  static const Color primary800 = Color(0xFF4F3A1D);
+  static const Color primary900 = Color(0xFF251C0E);
+  static const Color primary950 = Color(0xFF130E07);
+
+  // Brick Red palette
+  static const Color brickRed50 = Color(0xFFF8EDED);
+  static const Color brickRed100 = Color(0xFFF2DFDE);
+  static const Color brickRed200 = Color(0xFFE4BCB9);
+  static const Color brickRed300 = Color(0xFFD79D98);
+  static const Color brickRed400 = Color(0xFFC97A73);
+  static const Color brickRed500 = Color(0xFFBD5A52);
+  static const Color brickRed600 = Color(0xFF9F453D);
+  static const Color brickRed700 = Color(0xFF76332D);
+  static const Color brickRed800 = Color(0xFF51231F);
+  static const Color brickRed900 = Color(0xFF291210);
+  static const Color brickRed950 = Color(0xFF120807);
+
+  // Charcoal palette
+  static const Color charcoal50 = Color(0xFFF2F2F2);
+  static const Color charcoal100 = Color(0xFFE6E6E6);
+  static const Color charcoal200 = Color(0xFFC9C9C9);
+  static const Color charcoal300 = Color(0xFFB0B0B0);
+  static const Color charcoal400 = Color(0xFF949494);
+  static const Color charcoal500 = Color(0xFF7A7A7A);
+  static const Color charcoal600 = Color(0xFF616161);
+  static const Color charcoal700 = Color(0xFF454545);
+  static const Color charcoal800 = Color(0xFF2B2B2B);
+  static const Color charcoal900 = Color(0xFF141414);
+  static const Color charcoal950 = Color(0xFF0A0A0A);
+}
